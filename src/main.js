9d3d24ab44351748abd55c040f68011b2d4c3998
PiggySpeed <jlee14c@gmail.com>
2017-12-26 23:23:16 -0800

    circleci integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const { autoUpdater } = require('electron-updater');
 
 // production vs dev mode
 const IS_PRODUCTION = !!(process.env.NODE_ENV) && (process.env.NODE_ENV.trim() === 'production');
@@ -9,6 +10,7 @@ const IS_PRODUCTION = !!(process.env.NODE_ENV) && (process.env.NODE_ENV.trim() =
 let win;
 
 function createWindow() {
+  autoUpdater.checkForUpdatesAndNotify();
 
   // create the window
   win = new BrowserWindow({
@@ -55,4 +57,44 @@ app.on('activate', () => {
 
 });
 
+// ------------------------------------------------------------------
+// AUTO UPDATER CODE
+// ref: https://github.com/iffy/electron-updater-example/blob/master/main.js
+// ref: https://www.youtube.com/watch?v=_Hnzuko2eAc
+//
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
 
+function sendStatusToWindow(text) {
+  console.log("LOG:", text);
+  // log.info(text);
+  // win2.webContents.send('message', text);
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond
+    + ' - Downloaded ' + progressObj.percent + '%'
+    + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+  autoUpdater.quitAndInstall();
+});
